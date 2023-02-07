82b290ce19fd014af9887e340a9d13fdd042a018
BlakeJC94 <blakejamescook@gmail.com>
2023-02-07 22:06:23 +1100

    fix: ubound vars

--- lua/neozettel/utils.lua
+++ lua/neozettel/utils.lua
@@ -36,7 +36,7 @@ function M.edit_in_split(file_path, vert)
     vim.cmd.lcd(vim.fn.expand("%:p:h"))
 
     local n_lines = vim.api.nvim_buf_line_count(0)
-    local n_cols = #vim.api.nvim_buf_get_lines(0, line - 1, line, false)[1]
+    local n_cols = #vim.api.nvim_buf_get_lines(0, n_lines - 1, n_lines, false)[1]
     vim.api.nvim_win_set_cursor(0, {n_lines, n_cols})
 end
 
