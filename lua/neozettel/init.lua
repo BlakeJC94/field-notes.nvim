d5cb8c5ab2ee83145ceb4e517ec4e3fd7a2589c8
BlakeJC94 <blakejamescook@gmail.com>
2023-02-09 06:37:09 +1100

    fix: remove old imports

--- lua/neozettel/init.lua
+++ lua/neozettel/init.lua
@@ -11,12 +11,6 @@ end
 -- local note_functions = require("neozettel.note")
 -- M = vim.tbl_deep_extend("force", M, note_functions)
 
--- TODO remove once core command is set
-M.note = require("neozettel.core.note").note
-M.daily = require("neozettel.core.daily").daily
-M.weekly = require("neozettel.core.weekly").weekly
-M.monthly = require("neozettel.core.monthly").monthly
-
 M.neozettel = require("neozettel.core").neozettel
 
 return M
