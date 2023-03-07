c0627334659f4dda0abc1b75dc737a96a50a7870
BlakeJC94 <blakejamescook@gmail.com>
2023-03-07 12:36:02 +1100

    hotfix: invalid ref in notes.lua

--- lua/field_notes/core/note.lua
+++ lua/field_notes/core/note.lua
@@ -20,7 +20,7 @@ function M.note(keys)
     }, '/')
 
     utils.create_dir(notes_dir)
-    if M.buffer_is_in_field_notes() then
+    if utils.buffer_is_in_field_notes() then
         utils.add_field_note_link_at_cursor(utils.slugify(title))
     end
     utils.edit_note(notes_dir, title)
