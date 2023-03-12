d843ba13af0502469e5f401f64c9872d3292dd57
BlakeJC94 <blakejamescook@gmail.com>
2023-03-12 21:57:51 +1100

    feat: set augroups in init

--- lua/field_notes/init.lua
+++ lua/field_notes/init.lua
@@ -2,10 +2,12 @@ local M = {}
 
 local opts = require("field_notes.opts")
 local commands = require("field_notes.commands")
+local augroups = require("field_notes.augroups")
 
 function M.setup(config)
     opts.set(config or {})
     commands.set()
+    augroups.set()
 end
 
 return M
