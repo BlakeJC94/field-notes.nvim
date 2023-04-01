09610ae4d66de2a9a5d5ae6e0a4d5200326efa82
BlakeJC94 <blakejamescook@gmail.com>
2023-04-02 08:09:52 +1000

    revert: slugify flag to skip case

--- lua/field_notes/utils.lua
+++ lua/field_notes/utils.lua
@@ -1,11 +1,9 @@
 local M = {}
 
-function M.slugify(input_string, skip_lowercase)
+function M.slugify(input_string)
     local output_string = input_string
-    if not skip_lowercase then
-        output_string = string.lower(input_string)
-    end
-    output_string = string.gsub(output_string, '[ %[%]()%{%}%\\%/-.,=%\'%\":;><`]+', '_')
+    output_string = string.lower(output_string)
+    output_string = string.gsub(output_string, '[ _%[%]()%{%}%\\%/-.,=%\'%\":;><`]+', '_')
     output_string = string.gsub(output_string, '^[_]+', '')
     output_string = string.gsub(output_string, '[_]+$', '')
     return output_string
