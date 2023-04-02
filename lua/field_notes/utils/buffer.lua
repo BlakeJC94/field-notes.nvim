b7eca20fe63cd286d481c1f8b29d385174cbc5fd
BlakeJC94 <blakejamescook@gmail.com>
2023-04-02 14:23:18 +1000

    fix: get_git_branch_name -> get_git_branch

--- lua/field_notes/utils/buffer.lua
+++ lua/field_notes/utils/buffer.lua
@@ -43,7 +43,7 @@ function M.is_in_git_dir(buf_nr)
     return true
 end
 
-function M.get_git_branch_name(buf_nr)
+function M.get_git_branch(buf_nr)
     if not M.is_in_git_dir(buf_nr) then return nil end
     local buffer_dir = vim.fs.dirname(vim.api.nvim_buf_get_name(buf_nr))
     local cmd = table.concat({
